89b1d96be608e1ea0ad7ad06359cdde0144bdc03
Nishchayalds <nishchaysinghchandel@aladinnserver.in>
2025-04-22 10:09:24 +0530

    master

--- src/components/Telegram/Tgspinner.tsx
+++
@@ -1,7 +0,0 @@
-const TGSpinner = () => {
-  return (
-    <div className="w-16 h-16 border-5 border-t-9  border-t-transparent rounded-full animate-spin"></div>
-  );
-};
-
-export default TGSpinner;
